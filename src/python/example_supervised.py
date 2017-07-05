84ed6bc3a0cffeb105cc95625d85c700bf655313
三井 陽介 <mitsuiyosuke@LAB-N1110.local>
2017-07-05 21:28:34 +0900

    commit

---
+++ src/python/example_supervised.py
@@ -0,0 +1,34 @@
+'''
+Created on 2017/06/15
+
+'''
+import numpy as np
+from sklearn.datasets import fetch_mldata
+from supervised_bernoulli_mixture import SupervisedBernoulliMixture
+from PIL import Image
+import sys
+
+n_components = 4
+
+mnist = fetch_mldata('MNIST original', data_home=".")
+zero_index = mnist["data"] <= 128
+mnist["data"][zero_index] = 0
+mnist["data"][np.logical_not(zero_index)] = 1
+target_index = np.logical_or(np.logical_or(np.logical_or(mnist["target"] == 0, mnist["target"] == 1), mnist["target"] == 2), mnist["target"] == 3)
+shuffle_idx = range(mnist["data"][target_index].shape[0])
+np.random.shuffle(shuffle_idx)
+data = mnist["data"][target_index][shuffle_idx[:500]]
+target = mnist["target"][target_index][shuffle_idx[:500]]
+print "data.shape", data.shape
+data = data
+target = target
+n_dim = data.shape[1]
+
+poisson_mixture = SupervisedBernoulliMixture(n_components, 20)
+poisson_mixture.fit(data, target)
+print poisson_mixture.poi_params
+print "finish"
+for i, mean in enumerate(poisson_mixture.poi_params):
+    mean = np.array(mean)
+    pil_img = Image.fromarray(np.uint8(mean.reshape(28, 28) * 255))
+    pil_img.save('result' + str(i) + '.jpg')
--- src/python/supervised_bernoulli_mixture.py
+++ src/python/supervised_bernoulli_mixture.py
@@ -5,6 +5,7 @@ Created on 2017/06/12
 import numpy as np
 from scipy.misc import logsumexp
 import sys
+from scipy import optimize
 
 class SupervisedBernoulliMixture:
     
@@ -15,12 +16,15 @@ class SupervisedBernoulliMixture:
     def fit(self, sample_X, sample_y):
         self.sample_X = sample_X
         self.sample_y = sample_y
+        self.n_class = int(np.max(sample_y)) + 1
         n_samples = sample_X.shape[0]
         n_dimentions = sample_X.shape[1]
         self.poi_params = np.random.rand(self.n_components, n_dimentions) 
         self.latent_z = np.random.rand(n_samples, self.n_components)
         self.weights = np.random.rand(self.n_components)
         self.weights /= self.weights.sum()
+        self.supervived_params = np.random.rand(self.n_class, self.n_components)
+        self.slack_params = np.random.rand(n_samples)
         
         for _ in range(self.n_iter):
             self.eStep()
@@ -31,22 +35,39 @@ class SupervisedBernoulliMixture:
         np.clip(parameter, 1e-10, 1 - 1e-10, out=parameter)
         pdf = X * np.log(parameter) + (1 - X) * np.log((1 - parameter))
         return pdf.sum()
-     
+    
     def eStep(self):
-        self.latent_z = []
+        new_z = []
         log_weights = np.log(self.weights)
-        for X in self.sample_X:
-            weights_probs = [] 
+        for d in range(self.sample_X.shape[0]):
+            X = self.sample_X[d]
+            weights_probs = []
             for k in range(self.n_components):
                 a = log_weights[k]
                 b = self.log_bernoulli(X, self.poi_params[k])
-                weights_probs.append(a + b)
+                components_sum = np.sum(np.exp(np.dot(self.supervived_params, self.latent_z[d])) * self.supervived_params[:, k])
+                c = self.supervived_params[int(self.sample_y[d]), k] - 1 / self.slack_params[d] * components_sum
+                weights_probs.append(a + b + c)
             tot_log_likelyhood = logsumexp(weights_probs)
             z = []
-            for wp in weights_probs:  
+            for wp in weights_probs:
                 z.append(wp - tot_log_likelyhood)
-            self.latent_z.append(z)
-        self.latent_z = np.exp(np.array(self.latent_z))
+            
+            new_z.append(z)
+        self.latent_z = np.exp(np.array(new_z))
+        print "self.latent_z", self.latent_z.shape
+    
+    def gradient(self, theta, *args):
+        r = []
+        for i in range(self.n_class):
+            target_idx = self.sample_y == i
+            tmp = np.exp(np.dot(self.supervived_params[i], self.latent_z[target_idx].T)) * self.latent_z[target_idx].T
+            val = np.sum(self.latent_z[target_idx] - 1 / self.slack_params[target_idx].reshape(-1, 1) * tmp.T, 0)
+            r.append(val)
+        return np.array(r).flatten()
+        
+    def J(self, theta, *args):
+        return 0
     
     def mStep(self):
         new_poi_params = []
@@ -58,4 +79,11 @@ class SupervisedBernoulliMixture:
             new_poi_params.append(row_poi_params)
             self.weights[k] = tot_latent_z / self.sample_X.shape[0]
         self.poi_params = np.array(new_poi_params)
+        init_theta = self.supervived_params.flatten()
+        best_params = optimize.fmin_cg(self.J, init_theta, fprime=self.gradient)
+        self.supervived_params = best_params.reshape(self.supervived_params.shape[0], self.supervived_params.shape[1])
+        for d in range(self.slack_params.shape[0]):
+            self.slack_params[d] = np.sum(np.exp(np.dot(self.supervived_params, self.latent_z[d])))
+            
+            
         
