9a68e4640b54aeb94cc75edf44429fe61a96f37c
Y-Mitsui <dyfomg@gmail.com>
2017-08-03 19:35:53 +0900

    commit

---
+++ src/many_mixture/example2.py
@@ -0,0 +1,31 @@
+'''
+Created on 2017/07/20
+
+@author: mitsuiyosuke
+'''
+from many_mixture_wrap import ManyMixtureWrap
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+n_sample = 200
+n_poi_dim = 2
+n_components = 2
+#np.random.seed(1541)
+weights = np.random.dirichlet([1] * n_components)
+#weights = [0.1, 0.9]
+poi_params = np.random.uniform(1e-3, 2, size=(n_components, n_poi_dim))
+latent_z = np.random.multinomial(1, weights, size=n_sample)
+sample_poisson = []
+for z in latent_z:
+    X_poisson = np.random.poisson(poi_params[np.where(z==1)[0][0]], size=n_poi_dim)
+    sample_poisson.append(X_poisson)
+sample_poisson = np.array(sample_poisson)
+
+
+poisson_mixture = ManyMixtureWrap(n_components, 50)
+latent_z = poisson_mixture.fit_transform(sample_poisson)
+
+for poisson, color in zip(sample_poisson, latent_z):
+    plt.scatter(poisson[0], poisson[1], c=['r', 'b'][np.argmax(color)],alpha=0.1)
+plt.show()
